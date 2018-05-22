1688722cbcbe487b02c904b341fdbcb3874e7f66
Gokulakrishna <gokulakrishna@rocketmail.com>
2018-05-22 19:44:47 +0530

    Update background.js

--- background.js
+++ background.js
@@ -32,7 +32,7 @@ worker();
 loadworker.onmessage = function(e) {
   dict = e.data;
 loadworker.terminate();
-loadworker=undefined;
+//loadworker=undefined;
 }
 
 
@@ -43,12 +43,13 @@ function notify(request, sender) {
   // Searching  within 1.6 mil words will be default as it's safer
   // User can select top 300K only from settings page if she feels  searching across 1.6 mil is slow
   let searchsize = "TODO" // or default_1mil
-  let searchDict = [];
+  //let searchDict = [];
   if (searchsize === "default_300k") {
-    searchDict = dict.slice(0, 300001)
-  } else {
+    dict = dict.slice(0, 300001)
+  } 
+	/* else {
     searchDict = dict;
-  }
+  } */
   //let value=request.value;
   //var before = performance.now();
   //var k = searchDict.indexOf(value);
@@ -56,7 +57,7 @@ function notify(request, sender) {
   // var time = performance.now() - before;
   // console.log("time to search = "  + time + " ms");
 
-  if (searchDict.indexOf(request.value) > -1) {
+  if (dict.indexOf(request.value) > -1) {
     chrome.notifications.create({
       "type": "basic",
       "title": "Warning: Weak, Common Password",
@@ -70,29 +71,3 @@ function notify(request, sender) {
 
 
 chrome.runtime.onMessage.addListener(notify);
-
-/*
-
-var string = "This is my compression test.";
-console.log("Size of sample is: " + string.length);
-var compressed = LZString.compress(string);
-console.log("Size of compressed sample is: " + compressed.length);
-var string4 = LZString.decompress(compressed);
-console.log("Sample is: " + string4);
-
-var worker = new Worker(chrome.extension.getURL("readData.js"));
-
-let string2 = "";
-let absUrl = chrome.extension.getURL("data.array.js");
-var result;
-function load(){
-worker.postMessage([absUrl, 'other']);
-}
-load();
-worker.onmessage = function(e) {
-	console.log(e.data);
-  string2= e.data;
-  result=LZString.decompress(string2);
-  console.log(result);
-
-} */
